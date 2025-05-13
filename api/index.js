babf72de47eccac3c734f1851f73cbc646727581
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2025-05-13 18:17:06 +0100

    Add product id endpoint

--- api/index.js
+++ api/index.js
@@ -9,6 +9,13 @@ app.get("/products", (req, res) => {
   res.send(products);
 });
 
+app.get("/products/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const product = products.find((p) => p.id === id);
+  if (!product) res.status(404).json({ message: "product not found" });
+  res.send(product);
+});
+
 app.listen(3000, () => console.log("server running at port 3000"));
 
 module.exports = app;
