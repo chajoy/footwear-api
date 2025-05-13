efeec63cc8d05ebde50afff95b914dcedadc6ae7
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2025-05-13 15:21:43 +0100

    Add products json and img files

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const products = require("../products.json");
 
 app.get("/products", (req, res) => {
-  res.send("products");
+  res.send(products);
 });
 
 app.listen(3000, () => console.log("server running at port 3000"));
