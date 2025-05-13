4965ff0ba9cd840eb22e6c1c61c1e50957dabf83
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2025-05-13 15:10:04 +0100

    vercel function setup

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/products", (req, res) => {
+  res.send("products");
+});
+
+app.listen(3000, () => console.log("server running at port 3000"));
+
+module.exports = app;
